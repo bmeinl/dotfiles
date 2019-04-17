a3c8c1fe10594657adf83bec9a27fa3e76c03ad3
Benjamin Meinl <b.meinl@gmail.com>
2019-04-17 15:40:53 +0200

    Various changes to configs, added locale.conf and tmux.conf

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# tmux
+ln -fsv "$BASEDIR/runcom/tmux.conf" ~/.tmux.conf
+
 # vim
 ln -fsv "$BASEDIR/vim/vimrc" ~/.vimrc
 
@@ -16,3 +19,6 @@ ln -fsv "$BASEDIR/X11/xinitrc" ~/.xinitrc
 ln -fsv "$BASEDIR/X11/Xresources" ~/.Xresources
 ln -fsv "$BASEDIR/X11/XCompose" ~/.XCompose
 ln -fsv "$BASEDIR/X11/i3config" ~/.i3config
+
+# copy locale.conf to /etc/ if sudo is installed
+sudo cp "$BASEDIR/locale.conf" /etc/locale.conf
