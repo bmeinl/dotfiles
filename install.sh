5a5fd7a1250971d19029a3c6641bd69347596651
Ben Meinl <b.meinl@gmail.com>
2022-04-08 21:36:01 +0200

    Add more vim plugins, add settings for zsh, add interactive flag to install.sh ln's

--- install.sh
+++ install.sh
@@ -3,26 +3,26 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # tmux
-ln -fsv "$BASEDIR/runcom/tmux.conf" ~/.tmux.conf
+ln -fsvi "$BASEDIR/runcom/tmux.conf" ~/.tmux.conf
 
 # vim
-ln -fsv "$BASEDIR/vim/vimrc" ~/.vimrc
+ln -fsvi "$BASEDIR/vim/vimrc" ~/.vimrc
 
 # bash/readline
-ln -fsv "$BASEDIR/runcom/bashrc" ~/.bashrc
-ln -fsv "$BASEDIR/runcom/inputrc" ~/.inputrc
-ln -fsv "$BASEDIR/runcom/bash_profile" ~/.bash_profile
-ln -fsv "$BASEDIR/runcom/dir_colors" ~/.dir_colors
+ln -fsvi "$BASEDIR/runcom/bashrc" ~/.bashrc
+ln -fsvi "$BASEDIR/runcom/inputrc" ~/.inputrc
+ln -fsvi "$BASEDIR/runcom/bash_profile" ~/.bash_profile
+ln -fsvi "$BASEDIR/runcom/dir_colors" ~/.dir_colors
 
 # x11
-ln -fsv "$BASEDIR/X11/xinitrc" ~/.xinitrc
-ln -fsv "$BASEDIR/X11/Xresources" ~/.Xresources
-ln -fsv "$BASEDIR/X11/XCompose" ~/.XCompose
-ln -fsv "$BASEDIR/X11/i3config" ~/.i3config
+ln -fsvi "$BASEDIR/X11/xinitrc" ~/.xinitrc
+ln -fsvi "$BASEDIR/X11/Xresources" ~/.Xresources
+ln -fsvi "$BASEDIR/X11/XCompose" ~/.XCompose
+ln -fsvi "$BASEDIR/X11/i3config" ~/.i3config
 
 # copy locale.conf to /etc/ if sudo is installed
-sudo cp "$BASEDIR/locale.conf" /etc/locale.conf
+sudo cp -i "$BASEDIR/locale.conf" /etc/locale.conf
 
 # link tpm submodule
 mkdir -p ~/.tmux/plugins
-ln -fsv "$BASEDIR/tpm" ~/.tmux/plugins/tpm
+ln -fsvi "$BASEDIR/tpm" ~/.tmux/plugins/tpm
