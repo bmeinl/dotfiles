f2990dc8b45064fa616ee15ec94d500306733e1c
Ben Meinl <b.meinl@gmail.com>
2022-10-17 15:01:33 +0200

    Fix install.sh bugs

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ EOF
 }
 
 if [ $# -eq 0 ]; then
-    printHelp()
+    printHelp
     exit
 fi
 
@@ -28,36 +28,36 @@ done
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # tmux
-if [ "$ALL_flag" -eq 1 -o "$tmux_flag" -eq 1 ]; then
+if [ -n "$ALL_flag" -o -n "$tmux_flag" ]; then
     ln -fsvi "$BASEDIR/runcom/tmux.conf" ~/.tmux.conf
     mkdir -p ~/.tmux/plugins
     ln -fsvi "$BASEDIR/tpm" ~/.tmux/plugins/tpm
 fi
 
 # vim
-if [ "$ALL_flag" -eq 1 -o "$vim_flag" -eq 1 ]; then
+if [ -n "$ALL_flag" -o -n "$vim_flag" ]; then
     ln -fsvi "$BASEDIR/vim/vimrc" ~/.vimrc
 fi
 
 # bash
-if [ "$ALL_flag" -eq 1 -o "$bash_flag" -eq 1 ]; then
+if [ -n "$ALL_flag" -o -n "$bash_flag" ]; then
     ln -fsvi "$BASEDIR/runcom/bashrc" ~/.bashrc
     ln -fsvi "$BASEDIR/runcom/bash_profile" ~/.bash_profile
 fi
 
 # zsh
-if [ "$ALL_flag" -eq 1 -o "$zsh_flag" -eq 1 ]; then
+if [ -n "$ALL_flag" -o -n "$zsh_flag" ]; then
     ln -fsvi "$BASEDIR/runcom/zshrc" ~/.zshrc
 fi
 
 # readline
-if [ "$ALL_flag" -eq 1 -o "$readline_flag" -eq 1 ]; then
+if [ -n "$ALL_flag" -o -n "$readline_flag" ]; then
     ln -fsvi "$BASEDIR/runcom/inputrc" ~/.inputrc
     ln -fsvi "$BASEDIR/runcom/dir_colors" ~/.dir_colors
 fi
 
 # x11
-if [ "$ALL_flag" -eq 1 -o "$x11_flag" -eq 1 ]; then
+if [ -n "$ALL_flag" -o -n "$x11_flag" ]; then
     ln -fsvi "$BASEDIR/X11/xinitrc" ~/.xinitrc
     ln -fsvi "$BASEDIR/X11/Xresources" ~/.Xresources
     ln -fsvi "$BASEDIR/X11/XCompose" ~/.XCompose
@@ -66,7 +66,7 @@ if [ "$ALL_flag" -eq 1 -o "$x11_flag" -eq 1 ]; then
 fi
 
 # locale
-if [ "$ALL_flag" -eq 1 -o "$locale_flag" -eq 1 ]; then
+if [ -n "$ALL_flag" -o -n "$locale_flag" ]; then
     # copy locale.conf to /etc/ if sudo is installed
     sudo cp -i "$BASEDIR/locale.conf" /etc/locale.conf
 fi
